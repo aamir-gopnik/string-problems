ce930db440b85ed9e90eef2b128d7cc7dafcd65b
aamir-gopnik <aamir.gopnik@gmail.com>
2025-05-03 17:51:00 +0530

    added tesst cases

--- src/LongestSubStrWithNonRepeatingchars.java
+++ src/LongestSubStrWithNonRepeatingchars.java
@@ -1,19 +1,22 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class LongestSubStrWithNonRepeatingchars {
 
     public static void main(String[] args) {
-        String input = "abbac";
-        String output = "bac";
-
-        String input2 = "abcabcbb";
-        String output2 = "abc";
-
-        String input3 = "dvdf";
-        String output3 = "vdf";
-
-        System.out.println(longestSubStrLength(input));
+        String[] inputs = new String[]{"abcdabcbb","abbac","abcabcbb","dvdf"};
+        String[] output = new String[]{"abcd","bac","abc","vdf"} ;
+        int[] outputLength = new int[]{4,3,3,3};
+        int indx = 0;
+        for(String s : inputs){
+            int length = longestSubStrLength(s);
+            if(outputLength[indx] == length) {
+                System.out.println("Test case passed -> " + inputs[indx] + " -> " + length);
+            }
+            else {
+                System.out.println("Test case failed -> " + inputs[indx] + " -> " + length);
+            }
+            indx++;
+        }
     }
 
     //Solving using sliding window technique
@@ -21,7 +24,6 @@ public class LongestSubStrWithNonRepeatingchars {
         if(input.isEmpty())
             return 0;
         int lsLength = 0;
-        String longestSubStr = "";
         int start = 0;
         int end = 0;
         Set<Character> charSet = new LinkedHashSet<>();
@@ -29,14 +31,17 @@ public class LongestSubStrWithNonRepeatingchars {
             char ch = input.charAt(end);
             if(!charSet.contains(ch)) {
                 charSet.add(ch);
-                lsLength = Math.max(lsLength, charSet.size());
+                lsLength = Math.max(lsLength,charSet.size());
                 end++;
+                //System.out.println("state when no repeating char -> " + charSet);
             } else {
-                charSet.remove(Character.valueOf(input.charAt(start)));
+                charSet.remove(input.charAt(start));
                 start++;
+                //System.out.println("state when rep char -> " + charSet);
             }
         }
-        System.out.println(charSet);
         return lsLength;
     }
+
+
 }
